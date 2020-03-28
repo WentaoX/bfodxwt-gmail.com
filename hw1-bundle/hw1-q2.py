c0665cbdebebd7dbdee8f7e6bfc64809da58e622
WentaoX <bfodxwt@gmail.com>
2020-03-28 14:06:37 +0800

    finish 2.3 role discovery.

--- hw1-bundle/hw1-q2.py
+++ hw1-bundle/hw1-q2.py
@@ -1,5 +1,7 @@
 import snap
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def import_data(data_path = './hw1-q2.graph'):
     G = snap.TUNGraph.Load(snap.TFIn(data_path))
@@ -44,13 +46,8 @@ def create_basic_features(data_path = './hw1-q2.graph'):
     features[:, 2] = egonet_out
     return Graph, features
 
-def calculate_top_near_nodes(features, center_node, top_n):
-    '''
-    :param featuers: numpy array, each row is vector for each node
-    :param center_node: the node we want to compare with each other of the rest
-    :param top_n: return the number of most similar nodes
-    :return: a list of index
-    '''
+
+def calculate_similarity(features, center_node):
     similarity_score = np.dot(features, features[center_node, :])
     # normalize
     norm_center_node = np.linalg.norm(features[center_node, :])
@@ -62,6 +59,17 @@ def calculate_top_near_nodes(features, center_node, top_n):
     # replace nan with 0
     np.nan_to_num(similarity_score, copy=False)
     assert (similarity_score[center_node] == 1)  # self similarity should be 1
+    return similarity_score
+
+
+def calculate_top_near_nodes(features, center_node, top_n):
+    '''
+    :param featuers: numpy array, each row is vector for each node
+    :param center_node: the node we want to compare with each other of the rest
+    :param top_n: return the number of most similar nodes
+    :return: a list of index
+    '''
+    similarity_score = calculate_similarity(features, center_node)
     # the argsort() return sorted (ascending accroding to value) index,
     # thus select the ones in the latest part. But the self score should
     # be the largest, thus remove the self score.
@@ -81,6 +89,7 @@ def q2(features_xy, center_node=9, top_n=5):
     print(features_xy[nodes_id_top, :])
 
 
+# run q2.1
 Graph, features = create_basic_features()
 q2(features)
 
@@ -116,5 +125,61 @@ def recursieve_features(Graph, features, recursive_level):
     return features
 
 
+# run q2.2
 features_new = recursieve_features(Graph, features, 2)
 q2(features_new)
+
+
+def find_value_node(arr, num):
+    for node_id, score in enumerate(arr):
+        if round(score, 2) == num:
+            return node_id
+
+
+def role_discovery(features, center_node):
+    similarity_score = calculate_similarity(features, center_node)
+
+    # q3_1
+
+    plt.hist(similarity_score, bins=100)
+    plt.xlabel(f'Similarity with Node {center_node}')
+    plt.ylabel('Count')
+    plt.title('Similarity Score Distribution')
+    plt.legend()
+    plt.show()
+
+    # q3_2, select a 3 vector with similar score [000, 0.600, 0.85] seperately.
+
+    node_dict = {}
+    node_dict['dot0'] = find_value_node(similarity_score, 0)
+    node_dict['dot6'] = find_value_node(similarity_score, 0.6)
+    node_dict['dot85'] = find_value_node(similarity_score, 0.85)
+
+    print(node_dict)
+    for k in node_dict.keys():
+        print(f'Vecotor of Node {k} is:')
+        print(features[node_dict[k], :])
+        print('\n')
+    '''
+    {'dot0': 19, 'dot6': 42, 'dot85': 256}
+    Vecotor of Node dot0 is: # only publish paper himself
+    [0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0.
+     0. 0. 0.]
+
+    Vecotor of Node dot6 is: # only two author, both the two authors do not
+    cooperate with others
+    [1. 1. 0. 1. 1. 0. 1. 1. 0. 1. 1. 0. 1. 1. 0. 1. 1. 0. 1. 1. 0. 1. 1. 0.
+     1. 1. 0.]
+
+    Vecotor of Node dot85 is: # node have two other neigbours, two other neibours
+    cooperate with each other too, and two neigbours authour cooperate with other
+    authours too.
+    [2.          3.          2.          3.          5.         10.
+     6.         10.         20.          3.          5.         10.
+     5.66666667 14.33333333 14.         17.         43.         42.
+     6.         10.         20.         11.33333333 28.66666667 28.
+     34.         86.         84.]
+     
+     '''
+    
+role_discovery(features_new, 9)
